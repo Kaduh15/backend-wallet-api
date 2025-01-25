db46220b28d3ee370e8463b56ccce18e98d9c32d
Kadu <kadu.silva2014@gmail.com>
2025-01-25 07:08:10 -0300

    Implementa registro e login de usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomUserSerializer, CustomTokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [AllowAny]
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
